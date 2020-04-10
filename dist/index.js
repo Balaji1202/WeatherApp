2df16e59f3bfe229e16c42f69d9a152ba477ccb7
Balaji Saravanan <balajilitsv@gmail.com>
2020-04-11 02:39:07 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -738,7 +738,7 @@ exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader
 
 
 // module
-exports.push([module.i, "\n.footer[data-v-40df4999] {\n\t\tdisplay: flex;\n\t\tjustify-content: center;\n\t\tmargin: 100px 0 0 150px;\n}\n.footer-text[data-v-40df4999] {\n\t\ttext-align: center;\n\t\tfont-weight: bold;\n\t\tfont-size: 20px;\n\t\tfont-family: 'Courier New', Courier, monospace;\n}\n.avatar[data-v-40df4999] {\n    width: 150px;\n    height: 150px;\n    background-size: cover;\n    background-position: top center;\n    border-radius: 70%;\n}\n.avatar1[data-v-40df4999] {\n    background-image: url(" + escape(__webpack_require__(/*! ./../../assets/goalAvatar.png */ "./assets/goalAvatar.png")) + ");\n}\n", ""]);
+exports.push([module.i, "\n.footer[data-v-40df4999] {\n\t\tdisplay: flex;\n\t\tjustify-content: center;\n\t\tmargin: 100px 0 0 150px;\n}\n.footer-text[data-v-40df4999] {\n\t\ttext-align: center;\n\t\tfont-weight: bold;\n\t\tfont-size: 20px;\n\t\tfont-family: 'Courier New', Courier, monospace;\n}\n.avatar[data-v-40df4999] {\n    width: 150px;\n    height: 150px;\n    background-size: cover;\n    background-position: top center;\n    border-radius: 70%;\n}\n.avatar1[data-v-40df4999] {\n    background-image: url(./../assets/goalAvatar.png) + ");\n}\n", ""]);
 
 // exports
 
@@ -3031,7 +3031,7 @@ var staticRenderFns = [
             [
               _c("img", {
                 attrs: {
-                  src: __webpack_require__(/*! ./../../assets/linkedin.png */ "./assets/linkedin.png"),
+                  src: ./../assets/linkedin.png",
                   width: "50px",
                   height: "50px",
                   alt: ""
@@ -3048,7 +3048,7 @@ var staticRenderFns = [
             [
               _c("img", {
                 attrs: {
-                  src: __webpack_require__(/*! ./../../assets/mail.png */ "./assets/mail.png"),
+                  src: "./../assets/mail.png",
                   width: "40px",
                   height: "50px",
                   alt: ""
